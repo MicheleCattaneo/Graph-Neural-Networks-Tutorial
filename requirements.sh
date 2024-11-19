20664f2f76d14227742019c1323774cbaee9092d
MicheleCattaneo <cattami@usi.ch>
2024-11-19 22:36:55 +0100

    Added requirements

---
+++ requirements.sh
@@ -0,0 +1,8 @@
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.5.0+cu121.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.5.0+cu121.html
+pip install torch_geometric
+pip install torch-spatiotemporal
+pip install matplotlib
+pip install einops 
+pip install networkk
